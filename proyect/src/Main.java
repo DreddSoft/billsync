22af4d721d044ed77fa02b6829ddff1465b87c2c
NorthlandWow <d4n1.g0d0y@gmail.com>
2024-05-09 20:19:05 +0200

    actualizacion clase usuario

---
+++ proyect/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Usuario user1 = new Usuario();
+
+
+        user1.definirPassword("Antonio.33");
+
+        System.out.println(user1.getPassword());
+    }
+
+}
--- proyect/src/Usuario.java
+++ proyect/src/Usuario.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Usuario {
+public class Usuario implements Comparable<Usuario> {
 
     //variables
     private String nombre;
@@ -11,9 +11,7 @@ public class Usuario {
 
 
     //constructor
-    public Usuario(String nombre, String password) {
-        this.nombre = nombre;
-        this.password = password;
+    public Usuario() {
         this.balance = 0;
     }
 
@@ -52,11 +50,12 @@ public class Usuario {
         //comprobacion de si el nombre es valido, para ello verificamos que no haya numeros nicaracteres
 
         if (esNombreValido(nombre)) {//si el nombre es valido
-            this.nombre = nombre;//asignamos el nombre
+            this.setNombre(nombre);//asignamos el nombre
         } else {
             System.out.println("El nombre no es válido"); //si no es valido, mostramos mensaje
         }
     }
+
     //metodo privado que comprueba si el nombre es valido
     private boolean esNombreValido(String nombre) throws UserInvalidException {
         if (nombre.length() < 3 || nombre.length() > 20) { //suponemos que el nombre debe tener entre 3 y 20 caracteres
@@ -71,17 +70,25 @@ public class Usuario {
 
         return true; //si cumple todas las condiciones, es valido
     }
+
     //metodo que define la contraseña
-    public void definirPassword(String password) throws PassInvalidaException {
-        if (esContraValida(password)) { //si la contraseña es valida
-            this.password = password; //asignamos la contraseña
-        } else {
-            throw new PassInvalidaException("La contraseña no es válida");//si no es valida, lanzamos excepcion
+    public void definirPassword(String password) {
+        try {
+            if (esContraValida(password)) { //si la contraseña es valida
+                this.setPassword(password); //asignamos la contraseña
+                System.out.println("Contraseña correcta"); //mostramos mensaje
+            }else {
+                System.out.println("No se ha construido bien"); //lanzamos excepcion
+            }
+        } catch (PassInvalidaException e) { //capturamos la excepcion
+            System.out.println(e.getMessage()); //si no es valida, mostramos mensaje
         }
+
     }
+
     //metodo privado que comprueba si la contraseña es valida
     private boolean esContraValida(String pass) throws PassInvalidaException {
-        if (pass.length() < 8) { //suponemos que la contraseña debe tener al menos 8 caracteres
+        if (pass.length() < 8 || pass.length() >16) { //suponemos que la contraseña debe tener entre 8 y 16 caracteres
             return false;//si no tiene 8 caracteres, no es valida
         }
         boolean letraMayuscula = false; //suponemos que la contraseña debe tener al menos una letra mayuscula
@@ -113,11 +120,16 @@ public class Usuario {
         return true; //si cumple todas las condiciones, es valida
     }
 
-    public void generaGasto(double cantidad) {
-        this.balance -= cantidad;
+    public void generaGasto(double cantidad) { //metodo que genera un gasto
+        this.balance -= cantidad; //restamos la cantidad al balance
     }
 
-    public void generarPago(double cantidad) {
-        this.balance += cantidad;
+    public void generarPago(double cantidad) { //metodo que genera un pago
+        this.balance += cantidad; //sumamos la cantidad al balance
+    }
+
+    @Override
+    public int compareTo(Usuario o) { //metodo que compara dos usuarios
+        return this.nombre.compareTo(o.nombre); //comparamos los nombres
     }
 }
